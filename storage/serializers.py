abecb7c85ef8f0f0672a0ffa3e1a2f3c9ffa3787
Alexander Beshmelnitsyn <alexander.beshmelnitsyn@spetsservice.com>
2019-04-28 23:21:25 +0700

    commit

--- storage/serializers.py
+++ storage/serializers.py
@@ -1,13 +1,38 @@
 from rest_framework import serializers
 from rest_polymorphic.serializers import PolymorphicSerializer
 
+from product.models import ContainerParams
+from product.serializers import ProductSerializer
 from warehouse.serializers import WarehouseCellSerializer
-from .models import Storage
+from .models import Storage, Container
 from incoming.models import DocumentEntry
+
+
+class ContainerTypesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContainerParams
+        fields = (
+            'name',
+
+        )
+
+
+class ContainerSerializer(serializers.ModelSerializer):
+    cont_type = ContainerTypesSerializer(read_only=True)
+
+    class Meta:
+        model = Container
+        fields = (
+            'id',
+            'cont_type',
+            'barcode',
+        )
 
 
 class DocumentEntryViewSerializer(serializers.ModelSerializer):
     cell = WarehouseCellSerializer(read_only=True)
+    container = ContainerSerializer(read_only=True)
+    nomenclature = ProductSerializer(read_only=True)
 
     class Meta:
         model = DocumentEntry
@@ -15,6 +40,8 @@ class DocumentEntryViewSerializer(serializers.ModelSerializer):
             'id',
             'name',
             'cell',
+            'nomenclature',
+            'quantity',
             'container',
 
 
