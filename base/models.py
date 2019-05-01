77be531368f4a0a2debe7980dba5fb9ccb41a9a0
Alexander Beshmelnitsyn <alexander.beshmelnitsyn@spetsservice.com>
2019-05-01 22:47:08 +0700

    refactor import object

--- base/models.py
+++ base/models.py
@@ -5,8 +5,7 @@ class AbstractBase(models.Model):
     blocked = models.BooleanField(default=False,)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
-    from users.models import User
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey('users.User', on_delete=models.CASCADE, null=True)
 
     class Meta:
         abstract = True
