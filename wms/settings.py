cbb0b0ed2aa480f8bbdd87c70bb9bc69d950e19b
Alexander Beshmelnitsyn <alexander.beshmelnitsyn@spetsservice.com>
2019-10-03 12:23:42 +0700

    add corsheader

--- wms/settings.py
+++ wms/settings.py
@@ -93,22 +93,22 @@ WSGI_APPLICATION = 'wms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-##        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': '127.0.0.1', # set in docker-compose.yml
-        'PORT': 5432 # default postgres port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'HOST': '127.0.0.1', # set in docker-compose.yml
+#         'PORT': 5432 # default postgres port
+#     }
+# }
 
 
 # Password validation
